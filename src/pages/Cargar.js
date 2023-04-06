94b8eb42278b9008db27e9b126b5be53beec2308
ahiezer <ahiezerrivas@gmail.com>
2023-04-05 20:20:15 -0400

    Funcional ruta post

--- src/pages/Cargar.js
+++ src/pages/Cargar.js
@@ -1,33 +1,104 @@
 import React, { useState } from 'react'
 import  '../hojas-de-estilo/Formulario.css'
-
-import axio from 'axios'
+import  { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 
 function Cargar(){
 
+     const [testimonio, setTestimonio ] = useState('');
+    
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        
+        axios.post(/post/,{
+            nombre:data.nombre,
+            cargo:data.cargo,
+            pais:data.pais,
+            empresa:data.empresa,
+            testimonio:data.testimonio
+        })
+        .then((response)=> {
+            setTestimonio(response.data)
+        })
+       
+    }
+
+    
+
+
+ 
 
     return(
-    <div className='form-register'>
-        <h4>
-            Formulario para Cargar Testimonio
-        </h4>
-        <input className='controls' type='text' name='nombre' id='nombre' placeholder='Ingrese su nombre'>
-            
-        </input>
-        <input className='controls' type='text' name='cargo' id='cargo' placeholder='Ingrese su cargo'>
+        <div>
+            <h2>Formulario</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-        </input>
-        <input className='controls' type='text' name='pais' id='pais' placeholder='Ingrese su pais'>
+                <div>
+                    <label>Nombre</label>
+                    <input type='text' {...register('nombre', {
+                        required:true,
+                    })} />
+                </div>    
+                <div>
+                    <label>cargo</label>
+                    <input type='text'{...register('cargo', {
+                        required:true,
+                    })}  />
+                </div>    
+                <div>
+                    <label>Pais</label>
+                    <input type='text' {...register('pais', {
+                        required:true,
+                    })} />
+                </div>    
+                <div>
+                    <label>empresa</label>
+                    <input type='text' {...register('empresa', {
+                        required:true,
+                    })} />
+                </div>    
+                <div>
+                    <label>testimonio</label>
+                    <input type='textarea' {...register('testimonio', {
+                        required:true,
+                    })}  />
+                </div>    
+                <input className='botons pointer' type='submit' value='Registrar'  />
+            </form>
+        </div>
 
-        </input>
-        <input className='controls' type='text' name='testimonio' id='testimonio' placeholder='Ingrese su testimonio'>
+    // <div className='form-register'>
+    //     <form>
 
-        </input>
+    //         <h4>
+    //             Formulario para Cargar Testimonio
+    //         </h4>
+    //         <input className='controls' type='text' name='nombre' id='nombre' placeholder='Ingrese su nombre'>
+                
+    //         </input>
+    //         <input className='controls' type='text' name='cargo' id='cargo' placeholder='Ingrese su cargo'>
 
-        <input className='botons pointer' type='submit' value='Registrar'  />
+    //         </input>
+    //         <input className='controls' type='text' name='pais' id='pais' placeholder='Ingrese su pais'>
 
-    </div>
+    //         </input>
+    //         <input 
+    //             onClick={handleSubmit}
+    //             className='controls' 
+    //             type='text' 
+                
+                
+    //             placeholder='Ingrese su testimonio'>
+
+    //         </input>
+
+    //         <input className='botons pointer' type='submit' value='Registrar'  />
+    //     </form>
+
+    // </div>
     )
 }
 
